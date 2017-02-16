2bf9e433039700452c3c0f53fb37529df904d030
whuhan2013 <whuhan2013@163.com>
2017-02-16 12:48:45 +0800

    answer1

---
+++ answer1/cmake-build-debug/CMakeFiles/MachineLearingFoundation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MachineLearingFoundation.dir/Question20.cpp.o"
+  "MachineLearingFoundation.pdb"
+  "MachineLearingFoundation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MachineLearingFoundation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
